4a0e6f269bb862aed7c909095b328379728ff2bb
wangxiaobin <wangxb128@163.com>
2021-04-14 14:34:38 +0800

    添加雷霄骅simplest_librtmp_example-master学习项目原文件

---
+++ simplest_librtmp_example-master/simplest_librtmp_receive/simplest_librtmp_receive.cpp
@@ -0,0 +1,121 @@
+/**
+ * Simplest Librtmp Receive
+ *
+ * 雷霄骅，张晖
+ * leixiaohua1020@126.com
+ * zhanghuicuc@gmail.com
+ * 中国传媒大学/数字电视技术
+ * Communication University of China / Digital TV Technology
+ * http://blog.csdn.net/leixiaohua1020
+ *
+ * 本程序用于接收RTMP流媒体并在本地保存成FLV格式的文件。
+ * This program can receive rtmp live stream and save it as local flv file.
+ */
+#include <stdio.h>
+#include "librtmp/rtmp_sys.h"
+#include "librtmp/log.h"
+
+int InitSockets()
+{
+#ifdef WIN32
+	WORD version;
+	WSADATA wsaData;
+	version = MAKEWORD(1, 1);
+	return (WSAStartup(version, &wsaData) == 0);
+#endif
+}
+
+void CleanupSockets()
+{
+#ifdef WIN32
+	WSACleanup();
+#endif
+}
+
+int main(int argc, char* argv[])
+{
+	//初始化Socket 
+	InitSockets();
+	
+	double duration=-1;
+	int nRead;
+	//is live stream ?
+	bool bLiveStream=true;				
+	
+	
+	int bufsize=1024*1024*10;			
+	char *buf=(char*)malloc(bufsize);
+	memset(buf,0,bufsize);
+	long countbufsize=0;
+	
+	FILE *fp=fopen("receive.flv","wb");
+	if (!fp){
+		RTMP_LogPrintf("Open File Error.\n");
+		CleanupSockets();
+		return -1;
+	}
+	
+	/* set log level */
+	//RTMP_LogLevel loglvl=RTMP_LOGDEBUG;
+	//RTMP_LogSetLevel(loglvl);
+	//为结构体“RTMP”分配内存
+	RTMP *rtmp=RTMP_Alloc();
+	//初始化结构体“RTMP”中的成员变量
+	RTMP_Init(rtmp);
+	//set connection timeout,default 30s
+	rtmp->Link.timeout=10;	
+	// 设置输入的RTMP连接的URL 修改URL为：rtmp://58.200.131.2:1935/livetv/cctv1可用
+	if(!RTMP_SetupURL(rtmp,"rtmp://media3.scctv.net/live/scctv_800"))
+	{
+		RTMP_Log(RTMP_LOGERROR,"SetupURL Err\n");
+		RTMP_Free(rtmp);
+		CleanupSockets();
+		return -1;
+	}
+	if (bLiveStream){
+		rtmp->Link.lFlags|=RTMP_LF_LIVE;
+	}
+	
+	//1hour
+	RTMP_SetBufferMS(rtmp, 3600*1000);		
+	//建立RTMP连接，创建一个RTMP协议规范中的NetConnection
+	if(!RTMP_Connect(rtmp,NULL)){
+		RTMP_Log(RTMP_LOGERROR,"Connect Err\n");
+		RTMP_Free(rtmp);
+		CleanupSockets();
+		return -1;
+	}
+	//创建一个RTMP协议规范中的NetStream
+	if(!RTMP_ConnectStream(rtmp,0)){
+		RTMP_Log(RTMP_LOGERROR,"ConnectStream Err\n");
+		RTMP_Close(rtmp);
+		RTMP_Free(rtmp);
+		CleanupSockets();
+		return -1;
+	}
+	//从服务器读取数据
+	while(nRead=RTMP_Read(rtmp,buf,bufsize)){
+		fwrite(buf,1,nRead,fp);
+
+		countbufsize+=nRead;
+		RTMP_LogPrintf("Receive: %5dByte, Total: %5.2fkB\n",nRead,countbufsize*1.0/1024);
+	}
+
+	if(fp)
+		fclose(fp);
+
+	if(buf){
+		free(buf);
+	}
+
+	if(rtmp){
+		//关闭RTMP连接
+		RTMP_Close(rtmp);
+		//释放结构体“RTMP”
+		RTMP_Free(rtmp);
+		//关闭Socket
+		CleanupSockets();
+		rtmp=NULL;
+	}	
+	return 0;
+}
